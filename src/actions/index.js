c61bf2c50804960caba4f9b23c1be7710e63e74a
Max <maxnelson997@gmail.com>
2018-10-30 15:51:58 -0600

    redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,36 +2,21 @@ import {
     SET_STICKERS
 } from './types';
 
+import axios from 'axios';
+
 function fetchStickers() {
-    const stickers =  [
-        {
-          _id: 0,
-          category: 'JavaScript',
-          remaining: 4,
-          imageUrl: 'https://via.placeholder.com/80x80'
-        },
-        {
-          _id: 1,
-          category: 'Python',
-          remaining: 2,
-          imageUrl: 'https://via.placeholder.com/80x80'
-        },
-        {
-          _id: 2,
-          category: 'Ruby',
-          remaining: 6,
-          imageUrl: 'https://via.placeholder.com/80x80'
-        }
-      ]
-
-      const response = {
-          data: { stickers }
-      }
-
-      return ({
-          type: SET_STICKERS,
-          payload: response.data.stickers
-      })
+    return function(dispatch) {
+        axios.get('http://localhost:8080/api/store')
+            .then(response => {
+                dispatch ({
+                    type: SET_STICKERS,
+                    payload: response.data.stickers
+                })
+            })
+            .catch(err => {
+                if(err) { console.log(err) }
+            })
+    }
 }
 
 export { fetchStickers }
