73df60a59eabc7b7de6348875d9a60d6a80b8204
Max <maxnelson997@gmail.com>
2018-10-30 15:00:57 -0600

    redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+import { connect } from 'react-redux';
+
+class App extends Component {
   render() {
     return (
       <div className='app'>
-          
+        {
+          this.props.stickers.map(sticker => {
+            return (
+              <div className='sticker' key={sticker._id}>
+                  <img src={sticker.imageUrl}/>
+                  <div>Category: {sticker.category}</div>
+              </div>
+            )
+          }) 
+        }
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  // const stickers = state.store.stickers;
+  const { stickers } = state.store;
+  return { 
+    stickers
+  }
+}
+
+App = connect(mapStateToProps)(App);
+
+export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,4 +6,4 @@ const rootReducer = combineReducers({
   store
 });
 
-export default rootReducer;
+export default rootReducer;   
